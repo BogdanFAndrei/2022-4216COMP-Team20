b550e454c56b0f38d2e48cf9c829333621401985
BogdanFAndrei <98400889+BogdanFAndrei@users.noreply.github.com>
2022-03-14 12:50:50 +0000

    Program Menu

--- program.py
+++ program.py
@@ -1,41 +1,87 @@
-import sys
-import time
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
-#Options
-def optionA():
-    print("option A")
+# Creating tkinter window
+window = Tk()
+window.title("Climate and Weather Team 20")
+window.geometry('500x500')
+window.configure(background="darkgrey")
 
-def optionB():
-    pass
+ttk.Label(window, text = "Climate and weather Team 20", 
+          background = 'black', foreground ="white", 
+          font = ("Times New Roman", 15)).grid(row = 0, column = 1)
 
-def optionC():
-    print("The system will now exit")
-    time.sleep(2)
-    sys.exit()
+# label max/average/min
+ttk.Label(window, text = "Select the Year :",
+          font = ("Times New Roman", 10)).grid(column = 0,
+          row = 5, padx = 10, pady = 25)  
+# Combobox creation
+n = tk.StringVar()
+yearchoosen = ttk.Combobox(window, width = 27, textvariable = n)
+# Adding combobox drop down list
+yearchoosen['values'] = (' 2015', 
+                          ' 2016',
+                          ' 2017',
+                          ' 2018',
+                          ' 2019',
+                          ' 2020',
+                          ' 2021',)
+yearchoosen.grid(column = 1, row = 5)
+yearchoosen.current()
 
-#Main Menu Function
-def menu():
-    print("***Main Menu***")
-    time.sleep(1)
 
-    choice = input ("""
 
-                       A: Option 1 
-                       B: Option 2
-                       C: Quit
+# label month
+ttk.Label(window, text = "Select the Month :",
+          font = ("Times New Roman", 10)).grid(column = 0,
+          row = 6, padx = 10, pady = 25)
+# Combobox creation month
+n = tk.StringVar()
+monthchoosen = ttk.Combobox(window, width = 27, textvariable = n)
+  
+# Adding combobox drop down list
+monthchoosen['values'] = (' January', 
+                          ' February',
+                          ' March',
+                          ' April',
+                          ' May',
+                          ' June',
+                          ' July',
+                          ' August',
+                          ' September',
+                          ' October',
+                          ' November',
+                          ' December')
+  
+monthchoosen.grid(column = 1, row = 6)
+monthchoosen.current()
 
-                       Please make a selection from above:
-    
-    """)
-    
-    if choice == "A" or choice == "a":
-        optionA()
-    elif choice == "B" or choice == "b":
-        optionB()
-    elif choice == "C" or choice == "c":
-        optionC()
-    else:
-        print("Error Occured")
-        menu()
+# label year
+ttk.Label(window, text = "Select the  :",
+          font = ("Times New Roman", 10)).grid(column = 0,
+          row = 7, padx = 10, pady = 25)  
+# Combobox creation
+n = tk.StringVar()
+typevalue = ttk.Combobox(window, width = 27, textvariable = n)
+# Adding combobox drop down list
+typevalue['values'] = (' min', 
+                          ' avg',
+                          ' max')
+typevalue.grid(column = 1, row = 7)
+typevalue.current()
 
-menu()
+
+#Submitbutton function
+def click ():
+    MyLabel=Label(window, text="Look! I clicked on a Button!!")
+    MyLabel.pack
+#addbutton 
+Button(window,text="Submit", width=6, command=click) .grid(row=8,column=1,sticky=W)
+
+#add exit button
+Button(window,text="Quit", width=6, command=window.destroy) .grid(row=8,column=2,sticky=W)
+
+
+
+window.mainloop()
